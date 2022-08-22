e517f90d1001133ce73f56c1d8bf911306e446fd
Yunus Bozkurt <yunusbozkurt7000@gmail.com>
2022-08-22 20:43:03 +0100

    Create calendar booking tests

---
+++ tests/Feature/Calendar/BookingTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature\Calendar;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Inertia\Testing\Assert;
+use phpDocumentor\Reflection\Types\Void_;
+use Tests\TestCase;
+
+class BookingTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Initial seeder to create test data in database.
+     * 
+     * @var string
+     */
+    protected $seeder = CalendarBookingSeeder::class;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->User::factory()->create();
+    }
+    
+    /**
+     * Database testing on http responses from Inertia's Laravel adapter.
+     *
+     * @return void
+     */
+    public function test_can_view_calendar_bookings()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,19 +3,39 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+
+use function PHPUnit\Framework\assertEquals;
 
 class ExampleTest extends TestCase
 {
+    // use RefreshDatabase;        // database transaction executed with the testcase.
+    // use DatabaseMigrations;  // significantly slower but runs all migrations again.
+
     /**
-     * A basic test example.
+     * Indicates whether the defaul seeder should run before each test.
+     * 
+     * @var string
+     */
+    // protected $seeder = SeederClassNameSeeder::class;
+
+    /**
+     * Database testing using RefreshDatabase.
      *
      * @return void
      */
-    public function test_the_application_returns_a_successful_response()
-    {
-        $response = $this->get('/');
+    // public function test_the_application_returns_a_successful_response()
+    // {
+        // $user = User::factory()->create();
 
-        $response->assertStatus(200);
-    }
+        // $this->seed();
+
+        // $count = User::count();
+
+        // assertEquals(10, $count);
+    // }
 }
